0b6119ff3c48262d28ff104ae307cd1d05b49038
Thomas Foricher <thomas@reech.com>
2019-03-01 11:22:13 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,17 @@ const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
+  mode: 'production',
   entry: './lib/add.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'add.min.js',
     libraryTarget: 'umd',
     library: 'add',
-    // Workaround to fix umd build, restore webpack v3 behaviour
-    // https://github.com/webpack/webpack/issues/6677
-    // https://github.com/webpack/webpack/issues/6642
-    globalObject: "typeof self !== 'undefined' ? self : this"
   },
   module: {
     rules: [
-      {
-        test: /\.(js)$/,
-        use: 'babel-loader'
-      }
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
     ]
   },
   plugins: [
